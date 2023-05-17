71bb8018622bc2a2a24156180a0855cc791faf74
Md __Tanweer <110424884+iamtanweer07@users.noreply.github.com>
2023-05-17 23:39:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const options = {
      servers: [
        {
  //         url: "http://localhost:8080",
-             url: "https://nodejs-job-portal-app.onrender.com"
+             url: "https://nodejs-jobportal.onrender.com"
        },
      ],
    },
