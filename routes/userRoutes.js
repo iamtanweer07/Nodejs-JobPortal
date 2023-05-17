5861a39feddbf6ec0e06894542137f68bdf19bb0
Md Tanweer Alam <technoafshaan@gmail.com>
2023-05-17 23:00:56 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import userAuth from "../middelwares/authMiddleware.js";
+import { updateUserController } from "../controllers/userController.js";
+
+
+//router object
+const router = express.Router();
+
+
+//routes
+// GET USERS || GET
+
+// UPDATE USER || PUT
+router.put("/update-user",userAuth,updateUserController);
+
+
+export default router;
